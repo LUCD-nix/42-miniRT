7dd705324ea96691ba604e242b91363a2fe9abb9
Lucas Dupuy <lucascordu@gmail.com>
2026-01-10 15:41:09 +0100

    fix: main.c norminette refactor

--- src/main.c
+++ src/main.c
@@ -9,19 +9,13 @@
 /*   Updated: 2026/01/06 23:53:33 by hlongin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#define	M_PI 3.14159265358979323846
-
 #include "lighting/lighting.h"
 #include "vec3/vec3.h"
-#include "vec2/vec2.h"
-#include "mat3/mat3.h"
 #include "shapes/shapes.h"
 #include "rendering/rendering.h"
 #include "parsing/parsing.h"
 #include "../minilibx-linux/mlx.h"
 #include <stdio.h>
-#include <time.h>
 #include <math.h>
 #include "window/window.h"
 #include "keyboard/keyboard.h"
@@ -52,48 +46,31 @@ static t_camera	setup_camera(t_scene *parsed)
 
 	fov_rad = parsed->camera.fov * M_PI / 180.0;
 	return (camera_setup(
-		parsed->camera.position,
-		parsed->camera.orientation,
-		fov_rad
-	));
+			parsed->camera.position,
+			parsed->camera.orientation,
+			fov_rad));
 }
 
 int	main(int argc, char **argv)
 {
-	void		*mlx;
-	void		*window;
-	t_img		data;
-	t_scene		parsed;
-	t_camera	cam;
-	t_scene_lucas		scene;
-	clock_t		start;
-	clock_t		end;
+	t_img					data;
+	t_scene					parsed;
+	t_camera				cam;
+	static t_scene_lucas	scene = (t_scene_lucas){0};
 
 	if (argc != 2)
 		return (printf("Usage: %s <scene.rt>\n", argv[0]), 1);
-	
 	if (!parse_scene(argv[1], &parsed))
 		return (1);
-	init_window(&mlx, &window, &data);
+	init_window(&scene.mlx, &scene.mlx_window, &data);
 	setup_lights(&parsed, &parsed.shapes);
 	cam = setup_camera(&parsed);
-	printf("Camera pos: { %f, %f, %f }\n",
-		cam.camera_pos.x, cam.camera_pos.y, cam.camera_pos.z);
-	printf("Screen plane: { %f, %f, %f }\n",
-		cam.screen_plane.x, cam.screen_plane.y, cam.screen_plane.z);
-	scene = (t_scene_lucas){
-		.objs = &parsed.shapes,
-		.cam = &cam,
-		.mlx = mlx,
-		.mlx_window = window,
-		.img = &data
-	};
-	register_mlx_callbacks(window, &scene);
-	start = clock();
+	scene.objs = &parsed.shapes;
+	scene.cam = &cam;
+	scene.img = &data;
+	register_mlx_callbacks(scene.mlx_window, &scene);
 	full_render(&parsed.shapes, &cam, &data);
-	end = clock();
-	printf("Rendered in %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
-	mlx_put_image_to_window(mlx, window, data.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_put_image_to_window(scene.mlx, scene.mlx_window, data.img, 0, 0);
+	mlx_loop(scene.mlx);
 	return (0);
 }
--- src/rendering/full_render.c
+++ src/rendering/full_render.c
@@ -1,12 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   full_render.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlongin <hlongin@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/30 14:08:48 by lucorrei          #+#    #+#             */
+/*   Updated: 2026/01/06 23:53:33 by hlongin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "rendering.h"
+#include <stdio.h>
+#include <sys/time.h>
+
+static size_t	get_ms_time_diff(struct timeval start_time)
+{
+	struct timeval		current_time;
+	unsigned long int	res;
+
+	gettimeofday(&current_time, NULL);
+	res = (current_time.tv_sec - start_time.tv_sec) * 1000;
+	res += (current_time.tv_usec - start_time.tv_usec) / 1000;
+	return (res);
+}
 
 void	full_render(t_shapes *objs, t_camera *cam, t_img *data)
 {
-	size_t		i;
-	size_t		j;
-	t_vec3		rd;
-	t_colour	temp;
+	size_t			i;
+	size_t			j;
+	struct timeval	start_time;
+	t_vec3			rd;
+	t_colour		temp;
 
+	if (gettimeofday(&start_time, NULL) == -1)
+	{
+		printf("Fatal, couldn't get time of day\n");
+		return ;
+	}
 	i = -1;
 	while (++i < SCREEN_Y)
 	{
@@ -18,4 +48,6 @@ void	full_render(t_shapes *objs, t_camera *cam, t_img *data)
 			put_pixel_to_img(data, j, i, temp);
 		}
 	}
+	j = get_ms_time_diff(start_time);
+	printf("Finished rendering in %zu.%zu seconds\n", j / 1000, j % 1000);
 }
